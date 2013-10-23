db0082d5c9b560a194704da7b622e14eef2ad1ce
Thomas Ziegler <zieglert@fim.uni-passau.de>
2013-10-23 18:49:50 +0200

    added test for getMeasures in CubeManager

--- junit/de/uni_passau/fim/dimis/rest2sparql/cubemanagement/CubeManagerTest.java
+++ junit/de/uni_passau/fim/dimis/rest2sparql/cubemanagement/CubeManagerTest.java
@@ -41,4 +41,15 @@ public class CubeManagerTest {
 
         assertEquals(expected, res);
     }
+
+    @Test
+    public void testGetEntities() throws Exception {
+        String expected = "<?xml version='1.0' encoding='UTF-8'?>\n<sparql xmlns='http://www.w3.org/2005/sparql-results#'>\n\t<head>\n\t\t<variable name='CUBE_NAME'/>\n\t\t<variable name='DIMENSION_NAME'/>\n\t\t<variable name='ENTITY_NAME'/>\n\t\t<variable name='LABEL'/>\n\t</head>\n\t<results>\n\t\t<result>\n\t\t\t<binding name='CUBE_NAME'>\n\t\t\t\t<uri>http://code-research.eu/resource/Dataset-f744647d-e493-4640-9bd6-2080779a5e77</uri>\n\t\t\t</binding>\n\t\t\t<binding name='DIMENSION_NAME'>\n\t\t\t\t<uri>http://dbpedia.org/resource/River</uri>\n\t\t\t</binding>\n\t\t\t<binding name='ENTITY_NAME'>\n\t\t\t\t<uri>http://code-research.eu/resource/Entity_aed8cf79-ce59-4978-8259-8c9eed48889f</uri>\n\t\t\t</binding>\n\t\t\t<binding name='LABEL'>\n\t\t\t\t<literal datatype='http://www.w3.org/2001/XMLSchema#string'>Donau</literal>\n\t\t\t</binding>\n\t\t</result>\n\t</results>\n</sparql>\n";
+
+        CubeManager manager = new CubeManager(new CodeBigdataEngine());
+
+        String res = manager.getEntities("http://dbpedia.org/resource/River" ,"Dataset-f744647d-e493-4640-9bd6-2080779a5e77");
+
+        assertEquals(expected, res);
+    }
 }
