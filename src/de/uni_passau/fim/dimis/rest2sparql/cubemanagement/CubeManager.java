06764910794df2c0ae11ad399f35b3474b2aee61
Thomas Ziegler <zieglert@fim.uni-passau.de>
2013-10-23 15:24:10 +0200

    The CubeManager uses the new prefix stuff now; idea project file changes

--- src/de/uni_passau/fim/dimis/rest2sparql/cubemanagement/CubeManager.java
+++ src/de/uni_passau/fim/dimis/rest2sparql/cubemanagement/CubeManager.java
@@ -1,32 +1,50 @@
 package de.uni_passau.fim.dimis.rest2sparql.cubemanagement;
 
 import de.uni_passau.fim.dimis.rest2sparql.triplestore.TripleStoreConnection;
+import de.uni_passau.fim.dimis.util.PrefixCollection;
+import de.uni_passau.fim.dimis.util.SparqlPrefix;
 
 import java.io.IOException;
 
-import static de.uni_passau.fim.dimis.rest2sparql.triplestore.TripleStoreConnection.*;
+import static de.uni_passau.fim.dimis.rest2sparql.triplestore.TripleStoreConnection.OutputFormat;
 
 /**
  * Created with IntelliJ IDEA.
  * User: tommy
  * Date: 10/22/13
  * Time: 5:10 PM
- * To change this template use File | Settings | File Templates.
+ * <p/>
+ * This class is used to fetch information about the cubes in the database.
+ * The output is returned as an XML String by default.
  */
 public class CubeManager {
 
     private TripleStoreConnection connection;
     private OutputFormat format = OutputFormat.XML;
 
+    private PrefixCollection PrefixManager = new PrefixCollection();
+
+    /**
+     * Creat a new CubeManager.
+     *
+     * @param connection The connection to the triple store.
+     */
     public CubeManager(TripleStoreConnection connection) {
         this.connection = connection;
+        PrefixManager.addPrefix(new SparqlPrefix("qb", "http://purl.org/linked-data/cube#"));
+        PrefixManager.addPrefix(new SparqlPrefix("code", "http://code-research.eu/resource/"));
+        PrefixManager.addPrefix(new SparqlPrefix("rdfs", "http://www.w3.org/2000/01/rdf-schema#"));
     }
 
+    /**
+     * Returns a list of all cubes in the database. The list includes the unique cubename, and a label and description, if available.
+     *
+     * @return a list of all cubes in the database.
+     * @throws IOException If the connection to the database failes.
+     */
     public String getCubes() throws IOException {
 
-        String query = "PREFIX qb: <http://purl.org/linked-data/cube#> " +
-                "PREFIX code: <http://code-research.eu/resource/> " +
-                "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> " +
+        String query = PrefixManager.createPrefixString() +
                 "SELECT ?CUBE_NAME ?LABEL ?COMMENT. " +
                 "WHERE { ?CUBE_NAME a qb:DataSet. " +
                 "?CUBE_NAME rdfs:label ?LABEL. " +
@@ -36,11 +54,11 @@ public class CubeManager {
     }
 
     public String getDimensions(String CubeName) {
-        return "";
+        return ""; // TODO
     }
 
     public String getMeasures(String CubeName) {
-        return "";
+        return ""; // TODO
     }
 
     public void setOutputFormat(OutputFormat format) {
