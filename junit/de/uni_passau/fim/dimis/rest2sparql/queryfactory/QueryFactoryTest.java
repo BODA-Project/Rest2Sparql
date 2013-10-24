41b1bee84e417645b346b79758419b2717fd9e6e
Thomas Ziegler <zieglert@fim.uni-passau.de>
2013-10-24 13:54:56 +0200

    Added method to build simple queries

---
+++ junit/de/uni_passau/fim/dimis/rest2sparql/queryfactory/QueryFactoryTest.java
@@ -0,0 +1,37 @@
+package de.uni_passau.fim.dimis.rest2sparql.queryfactory;
+
+import de.uni_passau.fim.dimis.util.CubeObject;
+import de.uni_passau.fim.dimis.util.Dimension;
+import de.uni_passau.fim.dimis.util.Measure;
+import junit.framework.Assert;
+import org.junit.Test;
+
+import java.util.LinkedList;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: tommy
+ * Date: 10/24/13
+ * Time: 1:08 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class QueryFactoryTest {
+    @Test
+    public void testBuildObservationQuery() throws Exception {
+
+        String exp = "PREFIX code: <http://code-research.eu/resource/> PREFIX qb: <http://purl.org/linked-data/cube#> PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> SELECT ?OBS_NAME ?CUBE_NAME ?D_NAME_0 ?E_NAME_0 ?D_NAME_1 ?E_NAME_1 ?M_NAME_2 ?V_NAME_2 WHERE { ?OBS_NAME a qb:Observation. ?OBS_NAME ?D_NAME_0 ?E_NAME_0. ?OBS_NAME ?D_NAME_1 ?E_NAME_1. ?OBS_NAME ?M_NAME_2 ?V_NAME_2. ?OBS_NAME qb:dataSet ?CUBE_NAME. FILTER (?CUBE_NAME = code:Dataset-f744647d-e493-4640-9bd6-2080779a5e77). FILTER (?D_NAME_0 = <http://dbpedia.org/resource/Timestamp>). FILTER (?D_NAME_1 = <http://dbpedia.org/resource/River>). FILTER (?M_NAME_2 = <http://dbpedia.org/resource/Water_level>). }";
+
+        LinkedList<CubeObject> objects = new LinkedList<>();
+        Dimension d1 = new Dimension("Label", "http://dbpedia.org/resource/Timestamp");
+        Dimension d2 = new Dimension("Label", "http://dbpedia.org/resource/River");
+        Measure m1 = new Measure("Label", "http://dbpedia.org/resource/Water_level");
+        objects.add(d1);
+        objects.add(d2);
+        objects.add(m1);
+
+        QueryFactory f = new QueryFactory();
+        String res = f.buildObservationQuery("Dataset-f744647d-e493-4640-9bd6-2080779a5e77", objects);
+        Assert.assertEquals(exp, res);
+
+    }
+}
--- src/de/uni_passau/fim/dimis/rest2sparql/queryfactory/QueryFactory.java
+++ src/de/uni_passau/fim/dimis/rest2sparql/queryfactory/QueryFactory.java
@@ -1,5 +1,12 @@
 package de.uni_passau.fim.dimis.rest2sparql.queryfactory;
 
+import de.uni_passau.fim.dimis.util.CubeObject;
+import de.uni_passau.fim.dimis.util.FixedDimension;
+import de.uni_passau.fim.dimis.util.PrefixCollection;
+import de.uni_passau.fim.dimis.util.SparqlPrefix;
+
+import java.util.List;
+
 /**
  * Created with IntelliJ IDEA.
  * User: tommy
@@ -8,4 +15,60 @@ package de.uni_passau.fim.dimis.rest2sparql.queryfactory;
  * To change this template use File | Settings | File Templates.
  */
 public class QueryFactory {
+
+    private static final String OBSERVATION = "OBS_NAME";
+    private PrefixCollection PrefixManager = new PrefixCollection();
+
+    public QueryFactory() {
+        PrefixManager.addPrefix(new SparqlPrefix("qb", "http://purl.org/linked-data/cube#"));
+        PrefixManager.addPrefix(new SparqlPrefix("code", "http://code-research.eu/resource/"));
+        PrefixManager.addPrefix(new SparqlPrefix("rdfs", "http://www.w3.org/2000/01/rdf-schema#"));
+    }
+
+    public String buildObservationQuery(String cubeName, List<CubeObject> objects) {
+
+        generateVarNames(objects);
+
+        StringBuilder selectString = new StringBuilder("SELECT ?" + OBSERVATION + " ?CUBE_NAME ");
+        StringBuilder whereString = new StringBuilder("WHERE { ?" + OBSERVATION + " a qb:Observation. ");
+        StringBuilder filters = new StringBuilder("FILTER (?CUBE_NAME = code:" + cubeName + "). ");
+
+        for (CubeObject c : objects) {
+            for (String s : c.getAllVarNames()) {
+                // select string
+                selectString.append("?");
+                selectString.append(s);
+                selectString.append(" ");
+            }
+
+            // where string
+            whereString.append(c.buildPattern(OBSERVATION));
+
+            // filter string
+            filters.append(c.buildFilterString());
+            if (c instanceof FixedDimension) {
+                filters.append(((FixedDimension) c).buildEntityFilterString());
+            }
+        }
+        whereString.append("?" + OBSERVATION + " qb:dataSet ?CUBE_NAME. ");
+
+        StringBuilder query = new StringBuilder();
+        query.append(PrefixManager.createPrefixString());
+        query.append(selectString);
+        query.append(whereString);
+        query.append(filters);
+        query.append("}");
+
+        return query.toString();
+    }
+
+    private void generateVarNames(List<CubeObject> o) {
+
+        int i = 0;
+        for (CubeObject c : o) {
+            c.setVarName("NAME_" + Integer.toString(i));
+            i++;
+        }
+
+    }
 }
