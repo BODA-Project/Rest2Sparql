b6557254557e946a9dcf1a526062b48264eaec7f
Thomas Ziegler <zieglert@fim.uni-passau.de>
2013-11-03 15:02:01 +0100

    the main method starts the server at port 8080

--- src/de/uni_passau/fim/dimis/Main.java
+++ src/de/uni_passau/fim/dimis/Main.java
@@ -1,8 +1,15 @@
 package de.uni_passau.fim.dimis;
 
+import de.uni_passau.fim.dimis.rest2sparql.rest.Rest2SparqlServer;
+import de.uni_passau.fim.dimis.rest2sparql.rest.restadapter.RestAdapter;
+
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+
+        Rest2SparqlServer server = new Rest2SparqlServer(new RestAdapter(), 8080);
+        server.startServer();
     }
 }
