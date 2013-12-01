ea3b974fa29a31dafcc7a767b5b562cd113980a1
Thomas Ziegler <zieglert@fim.uni-passau.de>
2013-12-01 12:55:23 +0100

    Fixed test.

--- junit/de/uni_passau/fim/dimis/rest2sparql/queryfactory/QueryFactoryTest.java
+++ junit/de/uni_passau/fim/dimis/rest2sparql/queryfactory/QueryFactoryTest.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 
 import java.util.LinkedList;
 
+import static de.uni_passau.fim.dimis.rest2sparql.util.Parameters.*;
+
 /**
  * Created with IntelliJ IDEA.
  * User: tommy
@@ -14,6 +16,7 @@ import java.util.LinkedList;
  */
 public class QueryFactoryTest {
     @Test
+    @SuppressWarnings("deprecation")
     public void testBuildObservationQuery() throws Exception {
 
         String exp = "PREFIX qb: <http://purl.org/linked-data/cube#> PREFIX code: <http://code-research.eu/resource/> PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> SELECT ?OBS_NAME ?CUBE_NAME ?D_NAME_0 ?E_NAME_0 ?L_NAME_0 ?D_NAME_1 ?E_NAME_1 ?L_NAME_1 ?M_NAME_2 ?V_NAME_2 WHERE { ?OBS_NAME a qb:Observation. ?OBS_NAME ?D_NAME_0 ?E_NAME_0. ?E_NAME_0 <http://www.w3.org/2000/01/rdf-schema#label> ?L_NAME_0. ?OBS_NAME ?D_NAME_1 ?E_NAME_1. ?E_NAME_1 <http://www.w3.org/2000/01/rdf-schema#label> ?L_NAME_1. ?OBS_NAME ?M_NAME_2 ?V_NAME_2. ?OBS_NAME qb:dataSet ?CUBE_NAME. FILTER (?CUBE_NAME = code:Dataset-f744647d-e493-4640-9bd6-2080779a5e77). FILTER (?D_NAME_0 = <http://dbpedia.org/resource/Timestamp>). FILTER (?D_NAME_1 = <http://dbpedia.org/resource/River>). FILTER (?M_NAME_2 = <http://dbpedia.org/resource/Water_level>). }";
@@ -51,16 +54,16 @@ public class QueryFactoryTest {
     @Test
     public void testBuildObservationQuery_3() throws Exception {
 
-        String exp = "PREFIX qb: <http://purl.org/linked-data/cube#> PREFIX code: <http://code-research.eu/resource/> PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> SELECT ?D_NAME_0 ?E_NAME_0 ?L_NAME_0 ?D_NAME_1 ?E_NAME_1 ?L_NAME_1 ?M_NAME_2 ?V_NAME_2 ?OBS_NAME WHERE { ?OBS_NAME a qb:Observation. ?OBS_NAME ?D_NAME_0 ?E_NAME_0. ?E_NAME_0 <http://www.w3.org/2000/01/rdf-schema#label> ?L_NAME_0. ?OBS_NAME ?D_NAME_1 ?E_NAME_1. ?E_NAME_1 <http://www.w3.org/2000/01/rdf-schema#label> ?L_NAME_1. ?OBS_NAME ?M_NAME_2 ?V_NAME_2. ?OBS_NAME qb:dataSet ?C_NAME_3. FILTER (?D_NAME_0 = <http://dbpedia.org/resource/Timestamp>). FILTER (?D_NAME_1 = <http://dbpedia.org/resource/River>). FILTER (?M_NAME_2 = <http://dbpedia.org/resource/Water_level>). FILTER (?V_NAME_2 > \"1000\"). FILTER (?C_NAME_3 = <http://code-research.eu/resource/Dataset-f744647d-e493-4640-9bd6-2080779a5e77>). } GROUP BY ?D_NAME_1 ORDER BY ?D_NAME_0 LIMIT 100";
+        String exp = "PREFIX qb: <http://purl.org/linked-data/cube#> PREFIX code: <http://code-research.eu/resource/> PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> SELECT ?D_NAME_0 ?E_NAME_0 ?L_NAME_0 ?D_NAME_1 (SAMPLE(?E_NAME_1) AS ?E_NAME_1_AGG) (SAMPLE(?L_NAME_1) AS ?L_NAME_1_AGG) ?M_NAME_2 ?V_NAME_2 WHERE { ?OBS_NAME a qb:Observation. ?OBS_NAME ?D_NAME_0 ?E_NAME_0. ?E_NAME_0 <http://www.w3.org/2000/01/rdf-schema#label> ?L_NAME_0. ?OBS_NAME ?D_NAME_1 ?E_NAME_1. ?E_NAME_1 <http://www.w3.org/2000/01/rdf-schema#label> ?L_NAME_1. ?OBS_NAME ?M_NAME_2 ?V_NAME_2. ?OBS_NAME qb:dataSet ?C_NAME_3. FILTER (?D_NAME_0 = <http://dbpedia.org/resource/Timestamp>). FILTER (?D_NAME_1 = <http://dbpedia.org/resource/River>). FILTER (?M_NAME_2 = <http://dbpedia.org/resource/Water_level>). FILTER (?V_NAME_2 > \"1000\"). FILTER (?C_NAME_3 = <http://code-research.eu/resource/Dataset-f744647d-e493-4640-9bd6-2080779a5e77>). } GROUP BY ?D_NAME_1 ORDER BY ?D_NAME_0 LIMIT 100";
 
         LinkedList<CubeObject> objects = new LinkedList<>();
-        Dimension d1 = new Dimension("http://dbpedia.org/resource/Timestamp", new Parameters(true, 1, false, Parameters.AggregateFunction.NONE, null, null, null, null, null));
-        Dimension d2 = new Dimension("http://dbpedia.org/resource/River", new Parameters(true, 0, true, Parameters.AggregateFunction.NONE, null, null, null, null, null));
-        Measure m1 = new Measure("http://dbpedia.org/resource/Water_level", new Parameters(true, 0, false, Parameters.AggregateFunction.NONE, Parameters.Relation.BIGGER, "1000", null, null, null));
+        Dimension d1 = new Dimension("http://dbpedia.org/resource/Timestamp", new Parameters(true, 1, false, AggregateFunction.NONE, Relation.NONE, null, AggregateFunction.NONE, null, null));
+        Dimension d2 = new Dimension("http://dbpedia.org/resource/River", new Parameters(true, 0, true, AggregateFunction.NONE, Relation.NONE, null, AggregateFunction.NONE, null, null));
+        Measure m1 = new Measure("http://dbpedia.org/resource/Water_level", new Parameters(true, 0, false, AggregateFunction.NONE, Relation.BIGGER, "1000", AggregateFunction.NONE, null, null));
         objects.add(d1);
         objects.add(d2);
         objects.add(m1);
-        objects.add(new Cube("http://code-research.eu/resource/Dataset-f744647d-e493-4640-9bd6-2080779a5e77", new Parameters(false, 0, false, Parameters.AggregateFunction.NONE, null, null, null, null, null)));
+        objects.add(new Cube("http://code-research.eu/resource/Dataset-f744647d-e493-4640-9bd6-2080779a5e77", new Parameters(false, 0, false, AggregateFunction.NONE, Relation.NONE, null, AggregateFunction.NONE, null, null)));
 
         String res = QueryFactory.buildObservationQuery(new QueryDescriptor(objects, 100));
         Assert.assertEquals(exp, res);
