ad41417d6a960e6804fa756158bd9638df74fa16
Thomas Ziegler <zieglert@fim.uni-passau.de>
2013-12-01 12:55:50 +0100

    Added an additional unit test.

--- junit/de/uni_passau/fim/dimis/rest2sparql/queryfactory/QueryFactoryTest.java
+++ junit/de/uni_passau/fim/dimis/rest2sparql/queryfactory/QueryFactoryTest.java
@@ -69,4 +69,23 @@ public class QueryFactoryTest {
         Assert.assertEquals(exp, res);
 
     }
+
+    @Test
+    public void testBuildObservationQuery_4() throws Exception {
+
+        String exp = "PREFIX qb: <http://purl.org/linked-data/cube#> PREFIX code: <http://code-research.eu/resource/> PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> SELECT ?D_NAME_2 (SAMPLE(?E_NAME_2) AS ?E_NAME_2_AGG) (SAMPLE(?L_NAME_2) AS ?L_NAME_2_AGG) (SAMPLE(?V_NAME_3) AS ?V_NAME_AGG) WHERE { ?OBS_NAME a qb:Observation. ?OBS_NAME ?D_NAME_1 ?E_NAME_1. ?E_NAME_1 <http://www.w3.org/2000/01/rdf-schema#label> ?L_NAME_1. ?OBS_NAME ?D_NAME_2 ?E_NAME_2. ?E_NAME_2 <http://www.w3.org/2000/01/rdf-schema#label> ?L_NAME_2. ?OBS_NAME ?M_NAME_3 ?V_NAME_3. ?OBS_NAME qb:dataSet ?C_NAME_0. FILTER (?C_NAME_0 = <http://code-research.eu/resource/Dataset-f744647d-e493-4640-9bd6-2080779a5e77>). FILTER (?D_NAME_1 = <http://dbpedia.org/resource/Timestamp>). FILTER (?D_NAME_2 = <http://dbpedia.org/resource/River>). FILTER (?M_NAME_3 = <http://dbpedia.org/resource/Water_level>). } GROUP BY ?D_NAME_2 LIMIT 100";
+
+        LinkedList<CubeObject> objects = new LinkedList<>();
+        Dimension d1 = new Dimension("http://dbpedia.org/resource/Timestamp", new Parameters(false, 0, false, AggregateFunction.NONE, Relation.NONE, null, AggregateFunction.NONE, null, null));
+        Dimension d2 = new Dimension("http://dbpedia.org/resource/River", new Parameters(true, 0, true, AggregateFunction.NONE, Relation.NONE, null, AggregateFunction.NONE, null, null));
+        Measure m1 = new Measure("http://dbpedia.org/resource/Water_level", new Parameters(true, 0, false, AggregateFunction.SAMPLE, Relation.NONE, null, AggregateFunction.NONE, null, null));
+        objects.add(new Cube("http://code-research.eu/resource/Dataset-f744647d-e493-4640-9bd6-2080779a5e77", new Parameters(false, 0, false, AggregateFunction.NONE, Relation.NONE, null, AggregateFunction.NONE, null, null)));
+        objects.add(d1);
+        objects.add(d2);
+        objects.add(m1);
+
+        String res = QueryFactory.buildObservationQuery(new QueryDescriptor(objects, 100));
+        Assert.assertEquals(exp, res);
+
+    }
 }
