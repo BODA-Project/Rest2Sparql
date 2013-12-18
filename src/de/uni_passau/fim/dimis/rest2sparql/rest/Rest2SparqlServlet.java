f8b176d38d0b98a5f6ebb541de049f76da855c9b
Thomas Ziegler <zieglert@fim.uni-passau.de>
2013-12-18 14:55:19 +0100

    can handle POS and GET requests now, the result will be the same

--- src/de/uni_passau/fim/dimis/rest2sparql/rest/Rest2SparqlServlet.java
+++ src/de/uni_passau/fim/dimis/rest2sparql/rest/Rest2SparqlServlet.java
@@ -43,7 +43,7 @@ public class Rest2SparqlServlet extends HttpServlet {
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        doGet(request, response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
